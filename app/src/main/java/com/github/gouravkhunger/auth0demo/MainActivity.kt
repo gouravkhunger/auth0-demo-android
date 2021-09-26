5d9722216c3932868c142cc8ca3ae21e9b243660
Gourav Khunger <gouravkhunger18@gmail.com>
2021-09-26 16:07:21 +0530

    Auth0 Implemented.

--- app/src/main/java/com/github/gouravkhunger/auth0demo/MainActivity.kt
+++ app/src/main/java/com/github/gouravkhunger/auth0demo/MainActivity.kt
@@ -1,11 +1,120 @@
 package com.github.gouravkhunger.auth0demo
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationAPIClient
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.callback.Callback
+import com.auth0.android.provider.WebAuthProvider
+import com.auth0.android.result.Credentials
+import com.auth0.android.result.UserProfile
+import com.github.gouravkhunger.auth0demo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var account: Auth0
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Set up the account object with the Auth0 application details
+        account = Auth0(
+            "xljNBT32DCsVDN7sYDM1qEHMD2mA1fVp",
+            "auth0-demo-android.us.auth0.com"
+        )
+
+        binding.loginBtn.setOnClickListener {
+            WebAuthProvider.login(account)
+                .withScheme("demo")
+                .withScope("openid profile email")
+                // Launch the authentication passing the callback where the results will be received
+                .start(
+                    this,
+                    object : Callback<Credentials, AuthenticationException> {
+                        // Called when there is an authentication failure
+                        override fun onFailure(error: AuthenticationException) {
+                            // Something went wrong!
+                            Toast
+                                .makeText(this@MainActivity, "Login Error: \n${error.message}", Toast.LENGTH_LONG)
+                                .show()
+                        }
+
+                        // Called when authentication completed successfully
+                        override fun onSuccess(result: Credentials) {
+                            // Get the access token from the credentials object.
+                            // This can be used to call APIs
+                            val accessToken = result.accessToken
+                            showUserProfile(accessToken)
+                        }
+                    }
+                )
+        }
+        binding.logoutBtn.setOnClickListener {
+            logout()
+        }
+    }
+
+    private fun showUserProfile(accessToken: String) {
+        val client = AuthenticationAPIClient(account)
+
+        // With the access token, call `userInfo` and get the profile from Auth0.
+        client.userInfo(accessToken)
+            .start(object : Callback<UserProfile, AuthenticationException> {
+                override fun onFailure(error: AuthenticationException) {
+                    // Something went wrong!
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Error getting profile \n${error.message}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+                override fun onSuccess(result: UserProfile) {
+                    // We have the user's profile!
+                    Log.d("login result", result.toString())
+                    binding.nameTv.text = result.name
+                    binding.emailTv.text = result.email
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Login Successful!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
+    }
+
+    private fun logout() {
+        WebAuthProvider.logout(account)
+            .withScheme("demo")
+            .start(
+                this,
+                object : Callback<Void?, AuthenticationException> {
+                    override fun onSuccess(result: Void?) {
+                        // The user has been logged out!
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Successfully logged out!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        binding.nameTv.text = resources.getString(R.string.john_doe)
+                        binding.emailTv.text = resources.getString(R.string.email)
+                    }
+
+                    override fun onFailure(error: AuthenticationException) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Couldn't Logout!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            )
     }
 }
